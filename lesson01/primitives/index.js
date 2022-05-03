d8c30cf6955b7b1b5d53876a3e508a0a49ca7794
vitsovest <vitaliikom@gmail.com>
2022-05-03 11:26:16 +0300

    Lesson01 primitives

---
+++ lesson01/primitives/index.js
@@ -0,0 +1,50 @@
+console.log("primitives");
+
+//STRING
+let stringData = "string data";
+console.log(stringData);
+console.log(typeof stringData);
+
+stringData = "";
+console.log(stringData);
+console.log(typeof stringData);
+
+stringData = " ";
+console.log(stringData);
+console.log(typeof stringData);
+
+//NUMBER
+let number = 1;
+console.log(number);
+console.log(typeof number);
+
+number = 1e53;
+console.log(number);
+console.log(typeof number);
+
+number = Infinity;
+console.log(number);
+console.log(typeof number);
+
+number = NaN;
+console.log(number);
+console.log(typeof number);
+
+//Boolean
+let isOnline = true;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+isOnline = false;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+//undefined
+let message;
+console.log(message);
+console.log(typeof message);
+
+//null
+let user = null;
+console.log(user);
+console.log(typeof user);
