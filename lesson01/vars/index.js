761bc309156eb72bab059ac93510dd13b233f303
vitsovest <vitaliikom@gmail.com>
2022-05-03 10:50:08 +0300

    Lesson01 variables

---
+++ lesson01/vars/index.js
@@ -0,0 +1,29 @@
+console.log(student);
+
+var student = 'Vitalii';
+
+console.log(student);
+//console.log(number);
+
+var count;
+console.log(count);
+
+count = 1;
+console.log(count);
+count = 2;
+console.log(count);
+
+let number = 30;
+console.log(number);
+
+let age;
+console.log(age);
+age = 18;
+console.log(age);
+age = 22;
+console.log(age);
+
+const userName = 'Vova';
+console.log(userName);
+
+//userName = '';
