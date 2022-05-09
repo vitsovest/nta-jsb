ca1246ad6b8bcac6039ca95d14b7aacfc49a8acb
vitsovest <vitaliikom@gmail.com>
2022-05-09 09:26:24 +0300

    Lesson02 to task 05

---
+++ lesson02/js/task04.js
@@ -0,0 +1,10 @@
+console.log("TASK 04");
+
+let itemName = "Генератор защитного поля";
+let itemPrice = 1000;
+
+console.log(`'Выбран «Генератор защитного поля», цена за штуку ${itemPrice} кредитов'`);
+
+itemPrice = 2000;
+
+console.log(`'Выбран «Генератор защитного поля», цена за штуку ${itemPrice} кредитов'`);
