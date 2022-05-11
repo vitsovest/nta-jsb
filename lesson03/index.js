6d8f98730195548e2ca40c61e008156f373d85fb
vitsovest <vitaliikom@gmail.com>
2022-05-11 14:07:31 +0300

    Lesson03 to 13

--- lesson03/index.js
+++ lesson03/index.js
@@ -2,4 +2,6 @@ import "./js/task07.js";
 import "./js/task08.js";
 import "./js/task09.js";
 import "./js/task10.js";
-import "./js/task11.js";
+// import "./js/task11.js";
+// import "./js/task12.js";
+import "./js/task13.js";
