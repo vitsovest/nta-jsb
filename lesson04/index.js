2339019f05fca457112d1da1e699b6f2c1914d6a
vitsovest <vitaliikom@gmail.com>
2022-05-23 21:07:18 +0300

    Lesson 06

--- lesson04/index.js
+++ lesson04/index.js
@@ -1,3 +1,9 @@
-import "./js/task01.js";
+// import "./js/task01.js";
 // import "./js/task02.js";
 // import "./js/task03.js";
+
+const emojis = ['a','b','c'];
+console.log(emojis);
+console.log(emojis.map(x => x + 'a'));
+console.log(emojis.filter(x => x !== 'b'));
+console.log(emojis.reduce((acc, cur) => acc + 'c'));
