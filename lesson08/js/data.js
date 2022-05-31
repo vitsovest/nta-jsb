014d718fabdb76019ff2fb38e738838f7bef3b4b
vitsovest <vitaliikom@gmail.com>
2022-05-31 12:44:00 +0300

    Lesson07 data.js

---
+++ lesson08/js/data.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        id: 1,
+        image: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/producto1.png?alt=media&token=915bea59-e6c2-4474-bc72-abc1c47ef1c7',
+        name: 'Graphics card Gigabyte GTX 1650 D6 OC 4GB GDDR6',
+        descriptionShort: 'Graphics Processing GeForce® GTX 1650 Core Clock',
+        descriptionLarge: 'Powered by GeForce GTX 1650 NVIDIA Turing architecture and GeForce Experience Integrated with 4 GB GDDR6 128 bit WINDFORCE 2X memory interface Cooling system with reciprocating 80mm rotating fans single blade fans Core Clock: 1710 MHz',
+        price: 5000,
+        priceDiscount: 4399,
+        existence: true,
+        productCode: 'H410M-APRO'
+    },
+    {
+        id: 2,
+        image: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/producto2.png?alt=media&token=0d966e78-66be-43bd-a21b-98007831b9f6',
+        name: 'Hyperx Alloy origins RGB mechanical gaming keyboard',
+        descriptionShort: 'HyperX Alloy Origins Mechanical Tenkeyless for Gaming RGB',
+        descriptionLarge: 'It is a compact and robust keyboard that features mechanical switches designed to give gamers the best combination of style, performance and reliability. These mechanical switches feature exposed LEDs for impressive illumination with balanced actuation force and travel distance for high responsiveness and accuracy. Alloy Origins is built with a solid aluminum body that keeps it stiff and stable when the action calls for it, and lets you choose between three different levels of tilt. Its sleek and compact design leaves more room for mouse movement and also features a detachable USB Type-C cable for better portability.',
+        price: 1280,
+        existence: true,
+        productCode: 'AS13C-RAWQ'
+    },
+    {
+        id: 3,
+        image: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/producto3.png?alt=media&token=453593c9-9630-4de0-8ba4-a3a780a1cc6a',
+        name: 'Hyperx Cloud Stinger S 7.1 CH gaming headset with Mic USB',
+        descriptionShort: '2.4Ghz wireless connection and sturdy ear pads',
+        descriptionLarge: 'The HyperX Cloud Stinger™ headset is an officially licensed PS4 headset and is ideal for gamers looking for comfort, superior sound quality and increased convenience. They\'re lightweight and feature original HyperX memory foam, providing legendary comfort during marathon gaming sessions. Its 90-degree swivel ear cups can rest comfortably around your neck during breaks.',
+        price: 1348,
+        priceDiscount: 765,
+        existence: false,
+        productCode: '43REQ-TYI1'
+    },
+    {
+        id: 4,
+        image: 'https://firebasestorage.googleapis.com/v0/b/fotos-3cba1.appspot.com/o/producto4.png?alt=media&token=6c0ae77e-e48a-4b49-babc-0fc1023aa66a',
+        name: 'Motherboard Gigabyte B550M-DS3H Ryzen 3000 series Matx',
+        descriptionShort: 'Sopports AMD 3rd Gen Ryzen™ and 3rd Gen Ryzen™ with Radeon™ GPU',
+        descriptionLarge: 'GIGABYTE UD series motherboards use a pure digital 5 + 3 phase PWM + Low RDS(on) MOSFET design to support 3rd Generation AMD Ryzen™ CPUs by offering incredible precision in delivering power to the most powerful components. sensitive to power and motherboard power. As well as offering enhanced system performance and maximum hardware scalability.',
+        price: 2699,
+        existence: false,
+        productCode: 'TYUT7-FNTG'
+    }
+];
